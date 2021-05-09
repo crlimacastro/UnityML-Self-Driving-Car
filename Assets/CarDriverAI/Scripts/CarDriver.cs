5df055c4b819ef648ab915bee11bf59f8a20b66c
crlimacastro <crlimacastro@gmail.com>
2021-05-09 00:28:16 -0400

    Finished Setting Everything Up

--- Assets/CarDriverAI/Scripts/CarDriver.cs
+++ Assets/CarDriverAI/Scripts/CarDriver.cs
@@ -1,24 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CarDriver : MonoBehaviour
 {
 
     #region Fields
     private float speed;
-    private float speedMax = 70f;
-    private float speedMin = -50f;
-    private float acceleration = 30f;
-    private float brakeSpeed = 100f;
-    private float reverseSpeed = 30f;
+    private float speedMax = 20f;
+    private float speedMin = -20f;
+    private float acceleration = 25f;
+    private float brakeSpeed = 150f;
+    private float reverseSpeed = 15f;
     private float idleSlowdown = 10f;
 
     private float turnSpeed;
-    private float turnSpeedMax = 300f;
-    private float turnSpeedAcceleration = 300f;
-    private float turnIdleSlowdown = 500f;
-    private float minSpeedToTurn = 2f;
+    private float turnSpeedMax = 400f;
+    private float turnSpeedAcceleration = 500f;
+    private float turnIdleSlowdown = 700f;
 
     private float forwardAmount;
     private float turnAmount;
@@ -67,64 +64,64 @@ public class CarDriver : MonoBehaviour
 
         speed = Mathf.Clamp(speed, speedMin, speedMax);
 
-        Vector3 newVelocity = transform.forward * speed;
-        carRigidbody.velocity = new Vector3(newVelocity.x, carRigidbody.velocity.y, newVelocity.z);
+        carRigidbody.velocity = transform.forward * speed;
+        carRigidbody.velocity += Physics.gravity;
 
-        // Only turn if in movement
-        if (carRigidbody.velocity.magnitude > minSpeedToTurn)
+
+        if (speed < 0)
         {
-            if (speed < 0)
+            // Going backwards, invert wheels
+            turnAmount = turnAmount * -1f;
+        }
+
+        if (turnAmount > 0 || turnAmount < 0)
+        {
+            // Turning
+            if ((turnSpeed > 0 && turnAmount < 0) || (turnSpeed < 0 && turnAmount > 0))
             {
-                // Going backwards, invert wheels
-                turnAmount = turnAmount * -1f;
+                // Changing turn direction
+                float minTurnAmount = 20f;
+                turnSpeed = turnAmount * minTurnAmount;
             }
-
-            if (turnAmount > 0 || turnAmount < 0)
+            turnSpeed += turnAmount * turnSpeedAcceleration * Time.deltaTime;
+        }
+        else
+        {
+            // Not turning
+            if (turnSpeed > 0)
             {
-                // Turning
-                if ((turnSpeed > 0 && turnAmount < 0) || (turnSpeed < 0 && turnAmount > 0))
-                {
-                    // Changing turn direction
-                    float minTurnAmount = 20f;
-                    turnSpeed = turnAmount * minTurnAmount;
-                }
-                turnSpeed += turnAmount * turnSpeedAcceleration * Time.deltaTime;
+                turnSpeed -= turnIdleSlowdown * Time.deltaTime;
             }
-            else
+            if (turnSpeed < 0)
             {
-                // Not turning
-                if (turnSpeed > 0)
-                {
-                    turnSpeed -= turnIdleSlowdown * Time.deltaTime;
-                }
-                if (turnSpeed < 0)
-                {
-                    turnSpeed += turnIdleSlowdown * Time.deltaTime;
-                }
-                if (turnSpeed > -1f && turnSpeed < +1f)
-                {
-                    // Stop rotating
-                    turnSpeed = 0f;
-                }
+                turnSpeed += turnIdleSlowdown * Time.deltaTime;
             }
+            if (turnSpeed > -1f && turnSpeed < +1f)
+            {
+                // Stop rotating
+                turnSpeed = 0f;
+            }
+        }
 
-            float speedNormalized = speed / speedMax;
-            float invertSpeedNormalized = Mathf.Clamp(1 - speedNormalized, .75f, 1f);
+        float speedNormalized = speed / speedMax;
+        float invertSpeedNormalized = Mathf.Clamp(1 - speedNormalized, .75f, 1f);
 
-            turnSpeed = Mathf.Clamp(turnSpeed, -turnSpeedMax, turnSpeedMax);
+        turnSpeed = Mathf.Clamp(turnSpeed, -turnSpeedMax, turnSpeedMax);
 
-            carRigidbody.angularVelocity = new Vector3(carRigidbody.angularVelocity.x, turnSpeed * (invertSpeedNormalized * 1f) * Mathf.Deg2Rad, carRigidbody.angularVelocity.z);
+        carRigidbody.angularVelocity = new Vector3(0, turnSpeed * (invertSpeedNormalized * 1f) * Mathf.Deg2Rad, 0);
 
-            //if (transform.eulerAngles.x > 2 || transform.eulerAngles.x < -2 || transform.eulerAngles.z > 2 || transform.eulerAngles.z < -2)
-            //{
+        if (transform.eulerAngles.x > 2 || transform.eulerAngles.x < -2 || transform.eulerAngles.z > 2 || transform.eulerAngles.z < -2)
+        {
             transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-            //}
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        speed = Mathf.Clamp(speed, 0f, 20f);
+        if (collision.gameObject.tag == "vehicle" || collision.gameObject.tag == "building")
+        {
+            speed = Mathf.Clamp(speed, 0f, 20f);
+        }
     }
 
     public void SetInputs(float forwardAmount, float turnAmount)
--- Assets/Scripts/TrafficCarAgent.cs
+++ Assets/Scripts/TrafficCarAgent.cs
@@ -1,40 +1,43 @@
 ﻿using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using UnityEngine.SceneManagement;
 
 public class TrafficCarAgent : Agent
 {
-    [Header("Dependencies")]
-    public Transform target;
-
     private CarDriver carDriver;
-    private Vector3 initialPosition;
+    private PointPool spawnPointPool;
+    private PointPool targetPointPool;
+
+    private Transform currentTarget;
 
     // Init Logic (called once on Play)
     public override void Initialize()
     {
         carDriver = GetComponent<CarDriver>();
-        initialPosition = transform.position;
+        spawnPointPool = GameObject.Find("SpawnPoints").GetComponent<PointPool>();
+        targetPointPool = GameObject.Find("Targets").GetComponent<PointPool>();
     }
 
     // Episode reset logic (called at the start of every episode)
     public override void OnEpisodeBegin()
     {
-        // Go back to initial position
-        transform.position = initialPosition;
-
-        // Zero out all velocities
-        carDriver.StopCompletely();
+        Reset();
     }
 
     // Observations
     public override void CollectObservations(VectorSensor sensor)
     {
-        Vector3 vectorToTarget = target.position - transform.position;
+        Vector3 vectorToTarget = currentTarget.position - transform.position;
         // Normalized vector pointing to target
         Vector3 normDirectionToTarget = vectorToTarget.normalized;
 
         sensor.AddObservation(normDirectionToTarget); // Vector3, 3 observations
+
+        float distanceToTarget = vectorToTarget.magnitude;
+        sensor.AddObservation(distanceToTarget); // float, 1 observation
+
+        // Total observations: 4
     }
 
     // Actions
@@ -42,20 +45,17 @@ public class TrafficCarAgent : Agent
     {
         // vectorAction[0] == -1 back, 0 nothing, 1 forward
         // vectorAction[1] == -1 turn left, 0 nothing, 1 turn right
+
+        if (vectorAction[0] < 0)
+        {
+            AddReward(-0.01f); // Punish driving back too much
+        }
+
         carDriver.SetInputs(vectorAction[0], vectorAction[1]);
 
+        // Existential punishment (5000 if no MaxStep to avoid 0 division)
+        AddReward(-1f / (MaxStep == 0 ? MaxStep : 5000));
 
-        if (MaxStep != 0)
-        {
-            // Existential punishment
-            AddReward(-1f / MaxStep);
-        }
-        else
-        {
-            // Default to a punishment as if MaxStep was 5000
-            AddReward(-1f / 5000f);
-        }
-        
     }
 
     // Heuristic (player controlled)
@@ -69,9 +69,17 @@ public class TrafficCarAgent : Agent
 
     public void OnCollisionEnter(Collision collision)
     {
-        if (isBadCollision(collision))
+        if (IsBadCollision(collision))
         {
-            AddReward(-0.5f); // Heavy punishment
+            AddReward(-0.5f); // Heavy punishment for colliding with obstacles
+        }
+    }
+
+    public void OnCollisionStay(Collision collision)
+    {
+        if (IsBadCollision(collision))
+        {
+            AddReward(-0.1f); // Smaller punishment for staying
         }
     }
 
@@ -80,13 +88,30 @@ public class TrafficCarAgent : Agent
         if (collider.gameObject.tag == "target")
         {
             AddReward(1f); // Reward for reaching the target
+            targetPointPool.Return(currentTarget); // Return the target for other agents to go to
             EndEpisode();
         }
     }
 
     // Helper functions
-    public bool isBadCollision(Collision collision)
+    private bool IsBadCollision(Collision collision)
     {
-        return collision.gameObject.tag == "vehicle" || collision.gameObject.tag == "building" || collision.gameObject.tag == "sidewalk";
+        string tag = collision.gameObject.tag;
+        return tag == "vehicle" || tag == "building" || tag == "sidewalk" || tag == "props";
+    }
+
+    private void Reset()
+    {
+        // Zero out all velocities
+        carDriver.StopCompletely();
+
+        // Go to a random spawn point
+        Transform spawnPoint = spawnPointPool.FetchRandom();
+        transform.position = spawnPoint.position;
+        transform.rotation = spawnPoint.rotation;
+        spawnPointPool.Return(spawnPoint);
+
+        // Get a random target
+        currentTarget = targetPointPool.FetchRandom();
     }
 }
